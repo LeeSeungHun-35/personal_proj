65143874c9ab74656b8c97e7bc6bf960a1cc555a
LeeSeungHun-35 <ish0542@naver.com>
2024-09-13 10:43:10 +0900

    헤더와 컨텐츠 영역 분리(격리)

---
+++ src/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css';
+
+
+
+const Header = () => {
+    return (
+        <div className='Header'>
+            헤더영역
+        </div>
+    )
+}
+
+export default Header
---
+++ src/page/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Home.css';
+
+
+const Home = () => {
+  return (
+    <div className='Home'>홈(컨텐츠) 영역</div>
+  )
+}
+
+export default Home
