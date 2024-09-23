1435ff5c32ea96577ad53756d45d8571c02ccc9d
LeeSeungHun-35 <ish0542@naver.com>
2024-09-23 09:47:38 +0900

    Nav 스타일 변경

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 
-
 const Header = () => {
     return (
-        <div className='Header'>
-            <Link to = '/'> 홈</Link>
-            <Link to = '/Sym'>  병명</Link>
-        </div>
+        <nav className='Header'>
+            <div className='logo'>질병 예측</div>
+            <div className='nav-links'>
+                <Link to='/'>홈</Link>
+                <Link to='/Sym'>질병 확인</Link>
+            </div>
+        </nav>
     )
 }
 
-export default Header
+export default Header;
