756570749baf6675991229af617479d07a027fa8
LeeSeungHun-35 <ish0542@naver.com>
2024-09-23 13:47:37 +0900

    헤더 크기 조절하여 최적화

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <nav className='Header'>
             <div className='logo'>질병 예측</div>
             <div className='nav-links'>
-                <Link to='/'>홈</Link>
+                <Link to='/'>홈</Link>|
                 <Link to='/Sym'>질병 확인</Link>
             </div>
         </nav>
