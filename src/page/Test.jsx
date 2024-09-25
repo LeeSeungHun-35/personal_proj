4d089326d9b571cdab6180fa4aaa32cb24dd7904
LeeSeungHun-35 <ish0542@naver.com>
2024-09-25 16:37:11 +0900

    수정

--- src/page/Test.jsx
+++ src/page/Test.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import './Test.css'
 const Test = () => {
-  return (
-    <div>백엔드 연결 Test 페이지</div>
-  )
+    return (
+        <div className='Test'>
+            백엔드 연결 Test 페이지
+        </div>
+    )
 }
 
 export default Test
